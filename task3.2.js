3d2b0ccd095eba4b36bda3a9a5e6567e2e0b7bcb
Your Name <devbak1337@gmail.com>
2023-11-15 23:59:23 +0200

    dss

---
+++ task3.2.js
@@ -0,0 +1,7 @@
+const P = 7
+let i = 1
+while (i <= 10){
+
+    console.log(`${P} x ${i} = ${P*i}`)
+    i++
+}
---
+++ task3.js
@@ -0,0 +1,4 @@
+const two = 2
+for (let i = 1; i <= 10; i++) {
+    console.log(`${two} x ${i} = ${two*i}`)
+}
