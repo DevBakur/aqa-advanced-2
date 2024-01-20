1187cb03cc80875df94513f6d9dbac5929555e16
Your Name <devbak1337@gmail.com>
2024-01-20 18:52:41 +0200

    init commit

---
+++ task2.js
@@ -0,0 +1,8 @@
+const book = {
+    title: "Назва",
+    author: "Автор",
+    year: 2022
+};
+
+console.log("Назва:", book.title);
+console.log("Автор:", book.author);
---
+++ task3.js
@@ -0,0 +1,9 @@
+const car = {
+    brand: "Mini",
+    model: "Cooper One",
+    year: 2020
+};
+
+car.year = 2005;
+
+console.log(car);
